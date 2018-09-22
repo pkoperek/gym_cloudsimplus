281e477de0a983044892e658355593486df06008
Pawel Koperek <p.koperek@eyeo.com>
2018-09-22 15:54:53 +0200

    change the env test to use the array mode

--- manual_tests/test_run_env.py
+++ manual_tests/test_run_env.py
@@ -4,7 +4,7 @@ import gym_cloudsimplus
 env = gym.make('SingleDCAppEnv-v0')
 env.reset()
 for _ in range(5):
-    rendered = env.render()
+    rendered = env.render(mode='array')
     print("Rendered env length: " + str(len(rendered)) + " " + str(type(rendered)))
 
     observation, reward, done, info = env.step(env.action_space.sample())
