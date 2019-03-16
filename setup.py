f957f6fefdac33005685cc73db3352524637d75b
Pawel Koperek <pkoperek@gmail.com>
2019-03-16 23:35:56 +0100

    Switch the direnv layout to python3 Define packages so we can create a decent wheel

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='gym_cloudsimplus',
     version='0.0.1',
-    install_requires=['gym', 'py4j']
+    install_requires=['gym', 'py4j'],
+    packages=find_packages()
 )
