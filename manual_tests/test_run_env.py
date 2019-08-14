e6473f939d228ca8b7396643988999776ed89a0f
Pawel Koperek <pkoperek@gmail.com>
2019-08-14 22:53:51 +0200

    Add a parameter to control the number of initial VMs manually

--- manual_tests/test_run_env.py
+++ manual_tests/test_run_env.py
@@ -1,11 +1,17 @@
 import gym
 import gym_cloudsimplus
 import json
+import sys
 
 from read_swf import jobs
 
+if len(sys.argv) > 1:
+    initial_vm_count = sys.argv[1]
+else:
+    initial_vm_count = '10'
+
 env = gym.make('SingleDCAppEnv-v0',
-               initial_vm_count="10",
+               initial_vm_count=initial_vm_count,
                jobs_as_json=json.dumps(jobs),
                simulation_speedup="1000",
                split_large_jobs="true",
@@ -17,10 +23,10 @@ it = 0
 reward_sum = 0
 while True:
     observation, reward, done, info = env.step(0)
-    print(f'{it}, {observation}, {reward}')
+    print(f'{it}, {[str(i) for i in observation]}, {reward}')
     reward_sum += reward
 
     it += 1
     if done:
-        print("Episode finished! Reward sum: {reward_sum}")
+        print(f"Episode finished! Reward sum: {reward_sum}")
         break
